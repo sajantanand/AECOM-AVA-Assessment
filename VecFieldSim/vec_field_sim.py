41e753c005418ee7266dcfffbad11e6df8ee7b60
mcreng <tsehonam@gmail.com>
2018-08-06 15:57:56 +0800

    Updated codes with comments.

--- VecFieldSim/vec_field_sim.py
+++ VecFieldSim/vec_field_sim.py
@@ -11,10 +11,11 @@ from matplotlib.colors import LogNorm
 import progressbar as pbar
 import cv2
 
-empty = np.load('500/empty_new.npy')
-full = np.load('500/full_new.npy')
-base = np.load('500/base_new.npy')
-proposed = np.load('500/prop_new.npy')
+# Example code to load numpy arrays
+#empty = np.load('empty_new.npy')
+#full = np.load('full_new.npy')
+#base = np.load('base_new.npy')
+#proposed = np.load('prop_new.npy')
 
 class VecFieldSim:
     """
@@ -78,6 +79,7 @@ class VecFieldSim:
         p_dist = np.empty((0, 2))
         for _, sub_dat in enumerate(dat):
             length = len(sub_dat)
+            # Only concerns 80% of wind field (with reptition)
             ran_dat_1 = sub_dat[np.random.choice(sub_dat.shape[0], int(length*.8))]
             ran_dat_2 = sub_dat[np.random.choice(sub_dat.shape[0], int(length*.8))]
             curv = np.mean([ran_dat_1[:, -1], ran_dat_2[:, -1]], axis=0)
@@ -100,6 +102,7 @@ class VecFieldSim:
         p_dist = np.empty((0, 2))
         for _, sub_dat in enumerate(dat):
             length = len(sub_dat)
+            # Concerns 100% of wind field (with reptition)
             ran_dat_1 = sub_dat[np.random.choice(sub_dat.shape[0], int(length))]
             ran_dat_2 = sub_dat[np.random.choice(sub_dat.shape[0], int(length))]
             vdot = np.einsum('ij,ij->i', ran_dat_1[:, 4:7], ran_dat_2[:, 4:7])
@@ -166,13 +169,18 @@ class VecFieldSim:
             arr_p_max = []
             arr_d_max = []
             for _, dat in enumerate(self.arr_curv_dist):
+                # Select 0th ~ 65th percentile of data (curvature)
                 arr_p_min.append(np.percentile(dat[:, 0], 0))
                 arr_p_max.append(np.percentile(dat[:, 0], 65))
+                # Select up to 90th percentile of data (distance)
                 arr_d_max.append(np.percentile(dat[:, -1], 90))
+            # divide the ranges in 50x50
             bin_n = (np.linspace(0, max(arr_d_max), 50),
                      np.linspace(min(arr_p_min), max(arr_p_max), 50))
             for _, dat in enumerate(self.arr_curv_dist):
+                # calculates the histogram
                 hist = list(np.histogram2d(dat[:, -1], dat[:, 0], bins=bin_n))
+                # normalize it
                 hist[0] = (hist[0] / np.sum(hist[0])).astype(np.float32)
                 self.hist_curv_dist.append(hist)
         elif mode == 1:
@@ -181,13 +189,18 @@ class VecFieldSim:
             arr_p_max = []
             arr_d_max = []
             for _, dat in enumerate(self.arr_vel_dist):
+                # Select 10th ~ 90th percentile of data (velocity dot product)
                 arr_p_min.append(np.percentile(dat[:, 0], 10))
                 arr_p_max.append(np.percentile(dat[:, 0], 90))
+                # Select up to 90th percentile of data (distance)
                 arr_d_max.append(np.percentile(dat[:, -1], 90))
+            # divide the ranges in 50x50
             bin_n = (np.linspace(0, max(arr_d_max), 50),
                      np.linspace(min(arr_p_min), max(arr_p_max), 50))
             for _, dat in enumerate(self.arr_vel_dist):
+                # calculates the histogram
                 hist = list(np.histogram2d(dat[:, -1], dat[:, 0], bins=bin_n))
+                # normalize it
                 hist[0] = (hist[0] / np.sum(hist[0])).astype(np.float32)
                 self.hist_vel_dist.append(hist)
         else:
@@ -237,6 +250,8 @@ class VecFieldSim:
             for _, hist in enumerate(self.hist_curv_dist):
                 metric = np.append(metric, [
                     [cv2.compareHist(self.hist_curv_dist[0][0], hist[0], cv2.HISTCMP_CHISQR),
+                     # div 4 because cv2.HISTCMP_CHISQR_ALT
+                     # has a different scalar multiple (2 instead of 1/2)
                      cv2.compareHist(self.hist_curv_dist[0][0], hist[0], cv2.HISTCMP_CHISQR_ALT)/4,
                      cv2.compareHist(self.hist_curv_dist[0][0], hist[0], cv2.HISTCMP_BHATTACHARYYA)]
                     ], axis=0)
@@ -246,6 +261,8 @@ class VecFieldSim:
             for _, hist in enumerate(self.hist_vel_dist):
                 metric = np.append(metric, [
                     [cv2.compareHist(self.hist_vel_dist[0][0], hist[0], cv2.HISTCMP_CHISQR),
+                     # div 4 because cv2.HISTCMP_CHISQR_ALT
+                     # has a different scalar multiple (2 instead of 1/2)
                      cv2.compareHist(self.hist_vel_dist[0][0], hist[0], cv2.HISTCMP_CHISQR_ALT)/4,
                      cv2.compareHist(self.hist_vel_dist[0][0], hist[0], cv2.HISTCMP_BHATTACHARYYA)]
                     ], axis=0)
@@ -339,6 +356,7 @@ class VecFieldSim:
         if mode == 0:
             sup = np.max([np.max(hist[0]) for hist in self.hist_curv_dist])
             for i, hist in enumerate(self.hist_curv_dist):
+                # plot normalized histogram
                 cb = ax[i].imshow(hist[0].T[::-1],
                                   extent=[hist[1][0], hist[1][-1], hist[2][0], hist[2][-1]],
                                   aspect='auto', cmap=my_cmap, vmax=sup, norm=LogNorm())
@@ -346,6 +364,7 @@ class VecFieldSim:
         elif mode == 1:
             sup = np.max([np.max(hist[0]) for hist in self.hist_vel_dist])
             for i, hist in enumerate(self.hist_vel_dist):
+                # plot normalized histogram
                 cb = ax[i].imshow(hist[0].T[::-1],
                                   extent=[hist[1][0], hist[1][-1], hist[2][0], hist[2][-1]],
                                   aspect='auto', cmap=my_cmap, vmax=sup, norm=LogNorm())
@@ -369,7 +388,7 @@ class VecFieldSim:
 
         Returns:
             An array of means of metrics in format of
-            
+
                         D(0,0) D(0,1) D(0,2) ... D(0,n)
             CHISQR        *      *      *          *
             CHISQR_ALT    *      *      *          *
@@ -400,7 +419,7 @@ class VecFieldSim:
         axs[2].set_xlim(-0.03, 1.03)
 
         colors = ['gx', 'rx', 'bx', 'yx', 'mx', 'cx', 'kx']
-        
+
         means = np.zeros((3, self.data_cnt))
 
         widgets = [pbar.Percentage(), ' ',
@@ -410,13 +429,16 @@ class VecFieldSim:
         pro_bar.start()
         for k in range(n):
             pro_bar.update(k)
+            # calculate distances
             values = self.calc_dist(mode, calc_hist=True, show_bar=False)
+            # add to $means
             means += values.T
             for i in range(3):
                 for j in range(self.data_cnt):
+                    # plot the values
                     axs[i].plot(values[j, i], (self.data_cnt-j-1)*0.5, colors[j%len(colors)])
                     axs[i].set_title(metric_names[i])
-
+        # divide $n to the sum
         means /= n
 
         pro_bar.finish()
